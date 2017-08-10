9d98a068de46fea918ffe0ce0b404eb518b71794
dsanz <dsanz@plainconcepts.com>
2017-08-10 11:48:51 +0200

    Add initial solution and MVC5 template project.

---
+++ src/eShopCatalogMVC/Models/CatalogItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace eShopCatalogMVC.Models
+{
+    public class CatalogItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string PictureFileName { get; set; }
+
+        public string PictureUri { get; set; }
+
+        public int CatalogTypeId { get; set; }
+
+        public CatalogType CatalogType { get; set; }
+
+        public int CatalogBrandId { get; set; }
+
+        public CatalogBrand CatalogBrand { get; set; }
+
+        // Quantity in stock
+        public int AvailableStock { get; set; }
+
+        // Available stock at which we should reorder
+        public int RestockThreshold { get; set; }
+
+
+        // Maximum number of units that can be in-stock at any time (due to physicial/logistical constraints in warehouses)
+        public int MaxStockThreshold { get; set; }
+
+        /// <summary>
+        /// True if item is on reorder
+        /// </summary>
+        public bool OnReorder { get; set; }
+    }
+}
