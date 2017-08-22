286f09e9633d1001996da883eb5dcb4c06b551c4
dsanz <dsanz@plainconcepts.com>
2017-08-22 13:10:18 +0200

    Feedback

--- src/eShopLegacyMVC/Models/CatalogItem.cs
+++ src/eShopLegacyMVC/Models/CatalogItem.cs
@@ -12,29 +12,35 @@ namespace eShopCatalogMVC.Models
 
         // decimal(18,2)
         [RegularExpression(@"^\d+(\.\d{0,2})*$", ErrorMessage = "The field Price must be a positive number with maximum two decimals.")]
-        [Range(0, 9999999999999999.99)] 
+        [Range(0, 9999999999999999.99)]
         public decimal Price { get; set; }
 
+        [Display(Name = "Picture name")]
         public string PictureFileName { get; set; }
 
         public int CatalogTypeId { get; set; }
 
+        [Display(Name = "Type")]
         public CatalogType CatalogType { get; set; }
 
         public int CatalogBrandId { get; set; }
 
+        [Display(Name = "Brand")]
         public CatalogBrand CatalogBrand { get; set; }
 
         // Quantity in stock
         [Range(0, int.MaxValue)]
+        [Display(Name = "Stock")]
         public int AvailableStock { get; set; }
 
         // Available stock at which we should reorder
         [Range(0, int.MaxValue)]
+        [Display(Name = "Restock")]
         public int RestockThreshold { get; set; }
 
         // Maximum number of units that can be in-stock at any time (due to physicial/logistical constraints in warehouses)
         [Range(0, int.MaxValue)]
+        [Display(Name = "Max stock")]
         public int MaxStockThreshold { get; set; }
 
         /// <summary>
