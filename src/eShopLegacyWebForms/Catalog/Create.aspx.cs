1e58acc115079eb9b13f76c55e2a4bcab1271ca5
dsanz <dsanz@plainconcepts.com>
2017-08-30 10:01:09 +0200

    Add create page to web forms

---
+++ src/eShopLegacyWebForms/Catalog/Create.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace eShopLegacyWebForms.Catalog
+{
+    public partial class Create : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
