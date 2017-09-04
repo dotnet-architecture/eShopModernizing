c80c33d40f32655b4473b427a1288fe742802a4d
dsanz <dsanz@plainconcepts.com>
2017-09-04 10:33:43 +0200

    add modernized MVC solution

---
+++ eShopModernizedMVCSolution/src/eShopModernizedMVC/Controllers/CatalogController.cs
@@ -0,0 +1,153 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Web.Mvc;
+using eShopModernizedMVC.Models;
+using eShopModernizedMVC.Services;
+
+namespace eShopModernizedMVC.Controllers
+{
+    public class CatalogController : Controller
+    {
+        private ICatalogService service;
+
+        public CatalogController(ICatalogService service)
+        {
+            this.service = service;
+        }
+
+        // GET /[?pageSize=3&pageIndex=10]
+        public ActionResult Index(int pageSize = 10, int pageIndex = 0)
+        {
+            var paginatedItems = service.GetCatalogItemsPaginated(pageSize, pageIndex);
+            ChangeUriPlaceholder(paginatedItems.Data);
+            return View(paginatedItems);
+        }
+
+        // GET: Catalog/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CatalogItem catalogItem = service.FindCatalogItem(id.Value);
+            if (catalogItem == null)
+            {
+                return HttpNotFound();
+            }
+            AddUriPlaceHolder(catalogItem);
+
+            return View(catalogItem);
+        }
+
+        // GET: Catalog/Create
+        public ActionResult Create()
+        {
+            ViewBag.CatalogBrandId = new SelectList(service.GetCatalogBrands(), "Id", "Brand");
+            ViewBag.CatalogTypeId = new SelectList(service.GetCatalogTypes(), "Id", "Type");
+            return View(new CatalogItem());
+        }
+
+        // POST: Catalog/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Description,Price,PictureFileName,CatalogTypeId,CatalogBrandId,AvailableStock,RestockThreshold,MaxStockThreshold,OnReorder")] CatalogItem catalogItem)
+        {
+            if (ModelState.IsValid)
+            {
+                service.CreateCatalogItem(catalogItem);
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CatalogBrandId = new SelectList(service.GetCatalogBrands(), "Id", "Brand", catalogItem.CatalogBrandId);
+            ViewBag.CatalogTypeId = new SelectList(service.GetCatalogTypes(), "Id", "Type", catalogItem.CatalogTypeId);
+            return View(catalogItem);
+        }
+
+        // GET: Catalog/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CatalogItem catalogItem = service.FindCatalogItem(id.Value);
+            if (catalogItem == null)
+            {
+                return HttpNotFound();
+            }
+            AddUriPlaceHolder(catalogItem);
+            ViewBag.CatalogBrandId = new SelectList(service.GetCatalogBrands(), "Id", "Brand", catalogItem.CatalogBrandId);
+            ViewBag.CatalogTypeId = new SelectList(service.GetCatalogTypes(), "Id", "Type", catalogItem.CatalogTypeId);
+            return View(catalogItem);
+        }
+
+        // POST: Catalog/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Description,Price,PictureFileName,CatalogTypeId,CatalogBrandId,AvailableStock,RestockThreshold,MaxStockThreshold,OnReorder")] CatalogItem catalogItem)
+        {
+            if (ModelState.IsValid)
+            {
+                service.UpdateCatalogItem(catalogItem);
+                return RedirectToAction("Index");
+            }
+            ViewBag.CatalogBrandId = new SelectList(service.GetCatalogBrands(), "Id", "Brand", catalogItem.CatalogBrandId);
+            ViewBag.CatalogTypeId = new SelectList(service.GetCatalogTypes(), "Id", "Type", catalogItem.CatalogTypeId);
+            return View(catalogItem);
+        }
+
+        // GET: Catalog/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CatalogItem catalogItem = service.FindCatalogItem(id.Value);
+            if (catalogItem == null)
+            {
+                return HttpNotFound();
+            }
+            AddUriPlaceHolder(catalogItem);
+
+            return View(catalogItem);
+        }
+
+        // POST: Catalog/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CatalogItem catalogItem = service.FindCatalogItem(id);
+            service.RemoveCatalogItem(catalogItem);
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                service.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void ChangeUriPlaceholder(IEnumerable<CatalogItem> items)
+        {
+            foreach (var catalogItem in items)
+            {
+                AddUriPlaceHolder(catalogItem);
+            }
+        }
+
+        private void AddUriPlaceHolder(CatalogItem item)
+        {
+            item.PictureUri = this.Url.RouteUrl(PicController.GetPicRouteName, new { catalogItemId = item.Id }, this.Request.Url.Scheme);            
+        }
+    }
+}
---
+++ eShopModernizedMVCSolution/src/eShopModernizedMVC/Modules/ApplicationModule.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using eShopModernizedMVC.Models;
+using eShopModernizedMVC.Models.Infrastructure;
+using eShopModernizedMVC.Services;
+
+namespace eShopModernizedMVC.Modules
+{
+    public class ApplicationModule : Module
+    {
+        private bool useMockData;
+
+        public ApplicationModule(bool useMockData)
+        {
+            this.useMockData = useMockData;
+        }
+        protected override void Load(ContainerBuilder builder)
+        {
+            if (this.useMockData)
+            {
+                builder.RegisterType<CatalogServiceMock>()
+                    .As<ICatalogService>()
+                    .SingleInstance();
+            }
+            else
+            {
+                builder.RegisterType<CatalogService>()
+                    .As<ICatalogService>()
+                    .InstancePerLifetimeScope();
+            }
+
+            builder.RegisterType<CatalogDBContext>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<CatalogDBInitializer>()
+                .InstancePerLifetimeScope();
+
+            builder.RegisterType<CatalogItemHiLoGenerator>()
+                .SingleInstance();
+        }
+    }
+}
