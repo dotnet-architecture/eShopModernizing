f7904585c87f33c9e9eed0a8cd7c507326579325
Steve Smith <steve@kentsmiths.com>
2020-11-23 12:43:36 -0500

    migrated autofac

--- eShopLegacyMVCSolution/eShopPorted/Startup.cs
+++ eShopLegacyMVCSolution/eShopPorted/Startup.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using eShopPorted.Modules;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -12,9 +15,19 @@ namespace eShopPorted
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // Create Autofac container builder
+            var builder = new ContainerBuilder();
+            builder.Populate(services);
+            bool useMockData = true; // TODO: read from config
+            builder.RegisterModule(new ApplicationModule(useMockData));
+
+            ILifetimeScope container = builder.Build();
+
+            return new AutofacServiceProvider(container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
