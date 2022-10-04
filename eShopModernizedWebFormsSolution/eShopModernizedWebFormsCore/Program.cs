196885f01bec09e9e8499abcb36020d757abf2e9
Taylor Southwick <tasou@microsoft.com>
2022-10-04 13:01:56 -0700

    Add core project

---
+++ eShopModernizedWebFormsSolution/eShopModernizedWebFormsCore/Program.cs
@@ -0,0 +1,26 @@
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSystemWebAdapters();
+builder.Services.AddReverseProxy().LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+app.UseSystemWebAdapters();
+
+app.MapDefaultControllerRoute();
+app.MapReverseProxy();
+
+app.Run();
